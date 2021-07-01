dcc256da4451e675f5b35463a2098ae590f2339c
Whyzen <girard.paul39@gmail.com>
2021-07-01 10:54:41 +0200

    Updates

---
+++ precache-manifest.d635bc6259d1bed2edb4b2a9aef487a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00aa51c8bc68f056cfe9dcb8ab95c5c",
+    "url": "/Movie_App_React/index.html"
+  },
+  {
+    "revision": "2d80af6949215b6a4c8e",
+    "url": "/Movie_App_React/static/js/2.1212195b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie_App_React/static/js/2.1212195b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6596325cc115003285c",
+    "url": "/Movie_App_React/static/js/main.4fd5e2c9.chunk.js"
+  },
+  {
+    "revision": "6a858766894e8f33fc3a",
+    "url": "/Movie_App_React/static/js/runtime-main.e35944b7.js"
+  }
+]);
